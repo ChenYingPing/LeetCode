dd7485f4bb7da34b5a4fee23a35a6c4abd0c5b47
陈应平 <chenyingping@haodf.com>
2019-03-04 22:17:42 +0800

    Q26

--- C++/C++/Code.cpp
+++ C++/C++/Code.cpp
@@ -325,6 +325,19 @@ public:
         }
         return head;
     }
+    
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n<=1) return n;
+        
+        int pos=0;
+        for(int i=0; i<n-1; i++){
+            if (nums[i]!=nums[i+1]){
+                nums[++pos] = nums[i+1];
+            }
+        }
+        return pos+1;
+    }
 
 };
 
