03c4543fdf01adee15ca0a31855140f50ab8f060
陈应平 <chenyingping@haodf.com>
2019-03-12 08:05:22 +0800

    Q41

--- C++/C++/Code.cpp
+++ C++/C++/Code.cpp
@@ -452,6 +452,24 @@ public:
         return true;
     }
 
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        if (n<=0) return 1;
+        int num;
+        for(int i=0; i<n; i++) {
+            num = nums[i];
+            while (num>0 && num<n && nums[num-1]!=num) {
+                swap(nums[i], nums[num-1]);
+                num = nums[i];
+            }
+        }
+        for (int i=0; i<n; i++){
+            if (i+1 != nums[i]){
+                return i+1;
+            }
+        }
+        return n+1;
+    }
 };
 
 int main(int argc, const char * argv[]) {
