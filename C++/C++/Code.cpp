3d94e07865faa08466a544dd6497f603be23297f
陈应平 <chenyingping@haodf.com>
2019-03-13 08:30:46 +0800

    Q42

--- C++/C++/Code.cpp
+++ C++/C++/Code.cpp
@@ -470,6 +470,27 @@ public:
         }
         return n+1;
     }
+    
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size()-1;
+        int res = 0;
+        int maxleft = 0;
+        int maxright = 0;
+        while(left <= right){
+            if(height[left] <= height[right]){
+                if(height[left]>=maxleft) { maxleft = height[left]; }
+                else  { res += maxleft - height[left]; }
+                left += 1;
+            }
+            else{
+                if(height[right] >= maxright) { maxright = height[right]; }
+                else { res += maxright - height[right]; }
+                right -= 1;
+            }
+        }
+        return res;
+    }
 };
 
 int main(int argc, const char * argv[]) {
