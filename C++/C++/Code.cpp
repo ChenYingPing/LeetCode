cda99337933e3e040ef2faa3662352209bf6e6ff
陈应平 <chenyingping@haodf.com>
2019-03-19 08:22:48 +0800

    Q50、Q51

--- C++/C++/Code.cpp
+++ C++/C++/Code.cpp
@@ -577,6 +577,58 @@ public:
                 swap(matrix[i][j], matrix[j][i]);
         }
     }
+    double myPow(double x, int n) {
+        bool sign = false;
+        unsigned int exp = n;
+        if(n<0){
+            exp = -n;
+            sign = true;
+        }
+        double result = 1.0;
+        while (exp) {
+            if (exp & 1){
+                result *= x;
+            }
+            exp >>= 1;
+            x *= x;
+        }
+        
+        return sign ? 1/result : result;
+    }
+    
+    vector<vector<string> > solveNQueens(int n) {
+        vector<vector<string> > res;
+        vector<string> nQueens(n, string(n, '.'));
+        solveNQueens(res, nQueens, 0, n);
+        return res;
+    }
+    void solveNQueens(vector<vector<string> > &res, vector<string> &nQueens, int row, int &n) {
+        if (row == n) {
+            res.push_back(nQueens);
+            return;
+        }
+        for (int col = 0; col != n; ++col)
+            if (isValid(nQueens, row, col, n)) {
+                nQueens[row][col] = 'Q';
+                solveNQueens(res, nQueens, row + 1, n);
+                nQueens[row][col] = '.';
+            }
+    }
+    bool isValid(vector<string> &nQueens, int row, int col, int &n) {
+        //check if the column had a queen before.
+        for (int i = 0; i != row; ++i)
+            if (nQueens[i][col] == 'Q')
+                return false;
+        //check if the 45° diagonal had a queen before.
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; --i, --j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        //check if the 135° diagonal had a queen before.
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; --i, ++j)
+            if (nQueens[i][j] == 'Q')
+                return false;
+        return true;
+    }
 };
 
 int main(int argc, const char * argv[]) {
