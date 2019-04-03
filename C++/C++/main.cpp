5bbc7c9725b3b58ed033e0988ea32475728ce1f4
陈应平 <chenyingping@haodf.com>
2019-04-03 08:45:58 +0800

    Q79

--- C++/C++/main.cpp
+++ C++/C++/main.cpp
@@ -29,6 +29,45 @@ public:
             sub.pop_back();
         }
     }
+    
+    bool exist(vector<vector<char> > &board, string word) {
+        if (board.size()<=0 || word.size()<=0) return false;
+        int row = board.size();
+        int col = board[0].size();
+        for(int i=0; i<board.size(); i++) {
+            for(int j=0; j<board[i].size(); j++){
+                if ( board[i][j]==word[0]  ){
+                    if( exist(board, word, 0, i, j) ){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    
+    bool exist(vector<vector<char> > &board, string& word, int idx, int row, int col) {
+        if ( row<0 || row>=board.size() ||
+            col<0 || col>=board[0].size() ||
+            board[row][col] != word[idx]) {
+            return false;
+        }
+        if (idx+1 == word.size()) return true;
+        
+        //replace to a special char to avoid duplication.
+        board[row][col] = '\0';
+        
+        if ( exist(board, word, idx+1, row+1, col ) ||
+            exist(board, word, idx+1, row-1, col ) ||
+            exist(board, word, idx+1, row, col+1 ) ||
+            exist(board, word, idx+1, row, col-1 ) ) {
+            return true;
+        }
+        //restore the char
+        board[row][col] = word[idx];
+        
+        return false;
+    }
 };
 
 
