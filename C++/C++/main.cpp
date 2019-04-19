bc80d60b426c9a170b6a3a095d30f59f280afabf
陈应平 <1417alk@gmail.com>
2019-04-19 08:54:46 +0800

    Q92

--- C++/C++/main.cpp
+++ C++/C++/main.cpp
@@ -173,6 +173,69 @@ public:
         delete[] dp;
         return result;
     }
+    
+    ListNode *reverseBetween(ListNode *head, int m, int n) {
+        if(head == NULL || m >= n) return head;
+        ListNode dummy(0); // create a dummy node to mark the head of this list
+        dummy.next = head;
+        ListNode *pre = &dummy; // make a pointer pre as a marker for the node before reversing
+        for(int i = 0; i<m-1; i++) pre = pre->next;
+        
+        ListNode *start = pre->next; // a pointer to the beginning of a sub-list that will be reversed
+        ListNode *then = start->next; // a pointer to a node that will be reversed
+        
+        // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+        // dummy-> 1 -> 2 -> 3 -> 4 -> 5
+        
+        for(int i=0; i<n-m; i++)
+        {
+            start->next = then->next;
+            then->next = pre->next;
+            pre->next = then;
+            then = start->next;
+        }
+        
+        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+        
+        return dummy.next;
+    }
+    
+    ListNode *reverseBetweenII(ListNode *head, int m, int n) {
+        if (head==NULL || m>=n) return head;
+
+        ListNode fake(0);
+        fake.next = head;
+        ListNode *pBegin=&fake, *pEnd=&fake;
+        
+        int distance = n - m ;
+        while(pEnd && distance>0){
+            pEnd = pEnd->next;
+            distance--;
+        }
+        while(pBegin && pEnd && m-1>0) {
+            pBegin = pBegin->next;
+            pEnd = pEnd->next;
+            m--;
+        }
+        if (pBegin==NULL || pEnd==NULL || pEnd->next == NULL){
+            return head;
+        }
+        
+        ListNode *p = pBegin->next;
+        ListNode *q = pEnd->next->next;
+        
+        ListNode *pHead = q;
+        while(p != q){
+            ListNode* node = p->next;
+            p->next = pHead;
+            pHead = p;
+            p = node;
+        }
+        pBegin->next = pHead;
+        
+        return fake.next;
+    }
 };
 
 
