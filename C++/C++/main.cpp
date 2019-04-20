bdfa03236273d40236ff6fd3452baee88f412a02
陈应平 <1417alk@gmail.com>
2019-04-20 16:06:22 +0800

    Q93

--- C++/C++/main.cpp
+++ C++/C++/main.cpp
@@ -236,6 +236,39 @@ public:
         
         return fake.next;
     }
+ 
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> result;
+        string ip;
+        restoreIpAddressesHelper(s, 0, 0, ip, result);
+        return result;
+    }
+    void restoreIpAddressesHelper(string& s, int start, int partNum, string ip, vector<string>& result) {
+        
+        int len = s.size();
+        if ( len - start < 4-partNum  || len - start > (4-partNum)*3 ) {
+            return;
+        }
+        
+        if (partNum == 4 && start == len){
+            ip.erase(ip.end()-1, ip.end());
+            result.push_back(ip);
+            return;
+        }
+        
+        int num = 0;
+        for (int i=start; i<start+3; i++){
+            num = num*10 + s[i]-'0';
+            if (num<256){
+                ip+=s[i];
+                restoreIpAddressesHelper(s, i+1, partNum+1, ip+'.', result);
+            }
+            //0.0.0.0 valid, but 0.1.010.01 is not
+            if (num == 0) {
+                break;
+            }
+        }
+    }
 };
 
 
