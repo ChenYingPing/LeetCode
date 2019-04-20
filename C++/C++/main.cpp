2dcbd1300744d7ca0526a16d686177e2372786c7
陈应平 <1417alk@gmail.com>
2019-04-20 16:25:42 +0800

    Q93

--- C++/C++/main.cpp
+++ C++/C++/main.cpp
@@ -237,12 +237,6 @@ public:
         return fake.next;
     }
  
-    vector<string> restoreIpAddresses(string s) {
-        vector<string> result;
-        string ip;
-        restoreIpAddressesHelper(s, 0, 0, ip, result);
-        return result;
-    }
     void restoreIpAddressesHelper(string& s, int start, int partNum, string ip, vector<string>& result) {
         
         int len = s.size();
@@ -269,6 +263,27 @@ public:
             }
         }
     }
+    
+    vector<string> restoreIpAddresses(string s) {
+        vector<string> res;
+        int len = s.length();
+        for(int i = 1; i<4 && i<len-2; i++){
+            for(int j = i+1; j<i+4 && j<len-1; j++){
+                for(int k = j+1; k<j+4 && k<len; k++){
+                    string s1 = s.substr(0,i), s2 = s.substr(i,j-i), s3 = s.substr(j,k-j), s4 = s.substr(k,len-k);
+                    if(isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)){
+                        res.push_back(s1+"."+s2+"."+s3+"."+s4);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    bool isValid(string s){
+        if(s.length()>3 || s.length()==0 || (s[0]=='0' && s.length()>1) || atoi(s.c_str())>255)
+            return false;
+        return true;
+    }
 };
 
 
@@ -276,7 +291,8 @@ int main(int argc, const char * argv[]) {
     
     Solution slution;
     
-    int m=3, n=7;
+    vector<string> res = slution.restoreIpAddresses("25525511135");
+    
     
     return 0;
 }
