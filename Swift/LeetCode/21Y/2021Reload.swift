3faa81f669c51be6b4eee9e788ffc8c27614477c
陈应平 <peter.cyp@alibaba-inc.com>
2021-10-08 20:19:25 +0800

    code commit

--- Swift/LeetCode/21Y/2021Reload.swift
+++ Swift/LeetCode/21Y/2021Reload.swift
@@ -32,10 +32,12 @@ class ReloadLeetcode: UIViewController
         internal var val: Int
         internal var left: TreeNode?
         internal var right: TreeNode?
+        internal var parent: TreeNode?
         internal init(_ val: Int) {
             self.val = val
             self.left = nil
             self.right = nil
+            self.parent = nil
         }
     }
     
@@ -139,5 +141,148 @@ class ReloadLeetcode: UIViewController
         return found;
     }
     
+    func GetNext(pNode :TreeNode?) -> TreeNode? {
+        if (pNode == nil) {
+            return nil;
+        }
+        
+        var pNext:TreeNode? = nil;
+        if ((pNode?.right) != nil) {
+            var pRight = pNode?.right
+            while pRight?.left != nil {
+                pRight = pRight?.left
+            }
+            pNext = pRight
+        }
+        else if (pNode?.parent != nil) {
+            var pCurrent = pNode
+            var pParent = pNode?.parent
+            while (parent != nil && pCurrent?.val == pParent?.right?.val) {
+                pCurrent = pParent
+                pParent = pParent?.parent
+            }
+            
+            pNext = pParent
+        }
+        
+        return pNext;
+    }
     
+    func Fibonacci(n :Int) -> Int {
+        var fibN = 0
+        let result = [0,1]
+        if n < 2 {
+            return result[n]
+        }
+        var fibNMinusOne = 1
+        var fibNMinusTwo = 0
+        for _ in 2...n {
+            fibN = fibNMinusOne + fibNMinusTwo
+            fibNMinusTwo = fibNMinusOne
+            fibNMinusOne = fibN
+        }
+        
+        return fibN
+    }
+    
+    func SortAges( ages :inout [Int], length :Int) {
+        if ages.count <= 0 || length <= 0 {
+            return
+        }
+        let oldestAge = 99
+        var timesOfAge = [Int]()
+        
+        for i in 0...oldestAge {
+            timesOfAge[i] = 0
+        }
+        
+        for i in 0..<length {
+            let age = ages[i]
+            if age < 0 || age > oldestAge {
+                return
+            }
+            timesOfAge[age] += 1
+        }
+        
+        var index = 0
+        for i in 0...oldestAge {
+            for _ in 0..<timesOfAge[i] {
+                ages[index] = i
+                index += 1
+            }
+        }
+    }
+    
+    func Minxxx(numbers :[Int], length :Int) -> Int {
+        if numbers.count <= 0 || length <= 0  {
+            return -1
+        }
+        
+        var index1 = 0
+        var index2 = length - 1
+        var indexMid = index1
+        while numbers[index1] >= numbers[index2] {
+            if index2 - index1 == 1 {
+                indexMid = index2
+                break
+            }
+            indexMid = (index1 + index2) / 2
+            // 如果下标为 index1、index2 和 indexMid 指向的三个数字相等
+            // 则只能顺序查找
+            if numbers[index1] == numbers[index2] && numbers[indexMid] == numbers[index1] {
+                return -111 // 顺序查找的结果
+            }
+            
+            if numbers[indexMid] >= numbers[index1] {
+                index1 = indexMid
+            } else if numbers[indexMid] <= numbers[index2] {
+                index2 = indexMid
+            }
+        }
+        
+        return numbers[indexMid]
+    }
+    
+    func movingCount(threshold :Int, rows :Int, cols :Int) -> Int {
+        if threshold < 0 || rows <= 0 || cols <= 0 {
+            return 0
+        }
+        var visited = [Bool]()
+        for i in 0..<(rows*cols) {
+            visited[i] = false
+        }
+        
+        let count = movingCountCore(threshold: threshold, rows: rows, cols: cols, row: 0, col: 0, visited: &visited)
+        
+        return count
+        
+    }
+    
+    func movingCountCore(threshold :Int, rows :Int, cols :Int, row :Int, col :Int,  visited: inout [Bool]) -> Int {
+        var count = 0
+        if check(threshold: threshold, rows: rows, cols: cols, row: row, col: col, visited: visited) {
+            visited[row*cols + col] = true
+            count = 1 + movingCountCore(threshold: threshold, rows: rows, cols: cols, row: row, col: col, visited: &visited) + movingCountCore(threshold: threshold, rows: rows, cols: cols, row: row, col: col, visited: &visited) + movingCountCore(threshold: threshold, rows: rows, cols: cols, row: row, col: col, visited: &visited) + movingCountCore(threshold: threshold, rows: rows, cols: cols, row: row , col: col+1, visited: &visited)
+        }
+        return count
+    }
+    
+    func check(threshold: Int, rows: Int, cols: Int, row :Int, col :Int, visited :[Bool]) -> Bool {
+        var row = row
+        var col = col
+        if row >= 0 && row < rows && col >= 0 && col < cols && getDigitSum(number: &row) + getDigitSum(number: &col) <= threshold && !visited[row*cols+col] {
+            return true
+        }
+        return false
+    }
+    
+    func getDigitSum(number: inout Int) -> Int {
+        var sum = 0
+        while number > 0 {
+            sum += number % 10
+            number /= 10
+        }
+        
+        return sum
+    }
 }
